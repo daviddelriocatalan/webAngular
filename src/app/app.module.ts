36f7ea7b89e4be854007f3ede333418170aa4756
David <daviddelriocatalan@gmail.com>
2020-01-18 15:16:04 +0100

    Arreglada BarraNavegadora responsiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -20,7 +22,11 @@ import { EquipoComponent } from './equipo/equipo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
+  ],
+  exports:[
+    BarraNavegadoraComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/barra-navegadora/barra-navegadora.component.ts
+++ src/app/barra-navegadora/barra-navegadora.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraNavegadoraComponent implements OnInit {
 
+  public isMenuCollapsed = true;
+
   constructor() { }
 
   ngOnInit() {
